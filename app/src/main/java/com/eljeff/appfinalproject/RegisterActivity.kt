449d374b1cc4be9fcfca95be1237173dd661d659
jeferson-gallo <jdgap9801@gmail.com>
2021-09-06 20:33:46 -0500

    Login and register implemented

--- app/src/main/java/com/eljeff/appfinalproject/RegisterActivity.kt
+++ app/src/main/java/com/eljeff/appfinalproject/RegisterActivity.kt
@@ -2,32 +2,44 @@ package com.eljeff.appfinalproject
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.eljeff.appfinalproject.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var registerBinding: ActivityRegisterBinding
+
+
+    //inicialir firebase
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(registerBinding.root)
 
-
+        //Firebase
+        auth = Firebase.auth
 
         // Al presionar el boton de regsitrar
         registerBinding.registerButton.setOnClickListener {
 
             //Limpiar los erroes
             cleanErrors()
+            registerUser()
 
+            /*
             val userNameFrom = registerBinding.userNameRegEdTx.text.toString()
             val emailFrom = registerBinding.emailRegEdTx.text.toString()
             val passwordFrom = registerBinding.passwordRegEdTx.text.toString()
             val confPasswordFrom = registerBinding.confPasswordEdTx.text.toString()
 
-            //**************************************************************************************
+            //# **************************************************************************************
 
             // Verificamos campos vacios
             val nameNotempty = userNameFrom.isNotEmpty()
@@ -69,7 +81,7 @@ class RegisterActivity : AppCompatActivity() {
 
             val sent = nameNotempty && emailNotempty && acceptPassw && (passwordFrom == confPasswordFrom)
 
-            //**************************************************************************************
+            //# **************************************************************************************
 
             if (sent) {
                 // asignamos datos del formulario
@@ -94,6 +106,8 @@ class RegisterActivity : AppCompatActivity() {
                 // LLamar actividad de login
                 startActivity(intent)
             }
+
+             */
         }
 
         registerBinding.userNameRegEdTx.setOnClickListener {
@@ -109,6 +123,69 @@ class RegisterActivity : AppCompatActivity() {
             registerBinding.confPasswordRegTxInpLay.error = null
         }
 
+    }
+
+    private fun registerUser() {
+        val userName = registerBinding.userNameRegEdTx.text.toString()
+        val email = registerBinding.emailRegEdTx.text.toString()
+        val password = registerBinding.passwordRegEdTx.text.toString()
+        val repPassword = registerBinding.confPasswordEdTx.text.toString()
+
+        if(password != repPassword){
+            Toast.makeText(this, "Las contraseñas deben ser iguales",
+                Toast.LENGTH_SHORT
+            ).show()
+        }else{
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener() { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Register", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        goToLoginActivity()
+
+                    } else {
+                        var msg = ""
+                        // If sign in fails, display a message to the user.
+                        if(task.exception?.localizedMessage == "createUserWithEmail:success"){
+                            msg = "Usuario registrado exitosamente."
+                        }
+                        else if(task.exception?.localizedMessage == "The email address is badly formatted."){
+                            msg = "El correo está mal escrito."
+                        }
+                        else if(task.exception?.localizedMessage == "The given password is invalid. [ Password should be at least 6 characters ]"){
+                            msg = "La contraseña debe tener mínimo 6 caracteres"
+                        }
+                        else if(task.exception?.localizedMessage == "The email address is badly formatted."){
+                            msg = "El correo está mal escrito."
+                        }
+                        else if(task.exception?.localizedMessage == "The email address is already in use by another account."){
+                            msg = "Ya existe un usruario con éste correo electrónico"
+                        }
+
+
+                        Log.w("Register", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, msg,
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+        }
+
+    }
+
+    private fun goToLoginActivity() {
+        // Contenedor para tranferir datos entre actividades
+        val intent = Intent(this, LoginActivity::class.java)
+
+        // Limpiamos la pila de actividades
+        intent.addFlags(
+            Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        )
+
+        // LLamar actividad de login
+        startActivity(intent)
     }
 
     private fun cleanErrors() {
