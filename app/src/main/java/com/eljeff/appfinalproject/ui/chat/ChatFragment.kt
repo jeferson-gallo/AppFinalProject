8761da829f8f9bcec0dd86cd39aab4d130e2ccbd
jeferson-gallo <jdgap9801@gmail.com>
2021-09-11 16:49:30 -0500

    package fragments

--- app/src/main/java/com/eljeff/appfinalproject/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/eljeff/appfinalproject/ui/chat/ChatFragment.kt
@@ -1,18 +1,21 @@
-package com.eljeff.appfinalproject
+package com.eljeff.appfinalproject.ui.chat
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.eljeff.appfinalproject.R
 
-class PurchaseFragment : Fragment() {
+
+class ChatFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_purchase, container, false)
+        return inflater.inflate(R.layout.fragment_chat, container, false)
     }
+
 }
