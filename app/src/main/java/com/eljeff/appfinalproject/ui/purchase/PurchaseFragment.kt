3d0181731ae36e788bb722c55bd50f34c3b65c22
jeferson-gallo <jdgap9801@gmail.com>
2021-10-10 14:37:49 -0500

    separación de cart lists

--- app/src/main/java/com/eljeff/appfinalproject/ui/purchase/PurchaseFragment.kt
+++ app/src/main/java/com/eljeff/appfinalproject/ui/purchase/PurchaseFragment.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.eljeff.appfinalproject.data.server.ProductServer
 import com.eljeff.appfinalproject.databinding.FragmentPurchaseBinding
 import com.eljeff.appfinalproject.model.Users
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
@@ -21,6 +23,9 @@ class PurchaseFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var productAdapter: ProductAdapter
+
+    //inicialir firebase
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +44,8 @@ class PurchaseFragment : Fragment() {
             setHasFixedSize(false)
         }
 
+        //Firebase
+        auth = Firebase.auth
 
         // cargamos la lista de productos desde la base de datos
         loadFromServer()
@@ -48,12 +55,50 @@ class PurchaseFragment : Fragment() {
 
     private fun onProductItemClicked(product: ProductServer) {
 
+        val id = auth.currentUser?.uid
+
         // Instanciamos la base de datos
         val db = Firebase.firestore
+
+        val nmId: String = (product.name.toString()+"_"+id.toString())
+
+        //**************** nuevo *****************
+        nmId?.let {id -> db.collection("cart_list").document(id).set(product) }
+        //**************** Antiguo *****************
+
         // Add a new document with a generated ID
-        product.id?.let {id -> db.collection("cart_list").document(id).set(product) }
+        //product.id?.let {id -> db.collection("cart_list").document(id).set(product) }
+
+
+        // cambiarle el ID al producto
+        var documentUpdate = HashMap<String, Any>()
+        documentUpdate["id"] = id.toString()
+        db.collection("cart_list").document(nmId).update(documentUpdate)
+
+        updateAmount()
 
         Toast.makeText(requireContext(), "agregado - " + product.name, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateAmount() {
+        val db = Firebase.firestore
+        val id = auth.currentUser?.uid
+        var total: Long = 0
+
+        db.collection("cart_list").get().addOnSuccessListener { result ->
+
+            var listProducts: MutableList<ProductServer> = arrayListOf()
+
+            for (document in result){
+                val product: ProductServer = document.toObject<ProductServer>()
+                if (product.id == id) {
+                    total = total + product.cost!!
+                }
+            }
+
+        }
+        binding.totalTxVw.text = "Total: " + total.toString() + "$"
+
     }
 
     private fun loadFromServer() {
